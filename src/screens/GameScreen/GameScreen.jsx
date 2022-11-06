e2c92a70741f2045640707d062d5365680829acf
Alexandr Gradinar <alexandr.gradinar.02@gmail.com>
2022-11-06 09:18:00 +0200

    gh-pages set up

--- src/screens/GameScreen/GameScreen.jsx
+++ src/screens/GameScreen/GameScreen.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useContext } from "react";
 import { useNavigate } from "react-router-dom";
-import { config, rightAnswers } from "../../#config";
+import { config, rightAnswers } from "../../config";
 import Cell from "../../components/Cell/Cell";
 import { GameContext } from "../../context/context";
 import letters from "../../helpers/letters";
